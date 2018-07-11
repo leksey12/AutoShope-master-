0eb4f3c8031a348704bd393b6389d6f5a35850a7
Aleksey <Aleksey@ALEKSEY->
2018-07-11 13:44:59 +0400

    изменения

--- Domain/Concrete/EFDbContext.cs
+++ Domain/Concrete/EFDbContext.cs
@@ -10,7 +10,9 @@ using System.Data.Entity;
 namespace Domain.Concrete
 {
     public class EFDbContext : DbContext
-    {public DbSet<Skoda> Skodas { get; set; }
+{
+        public EFDbContext() : base("EFDbContext") { }
+        public DbSet<Skoda> Skodas { get; set; }
         public DbSet<Auto> Autos { get; set; }
         public DbSet<BMW> BMWs { get; set; }
         public DbSet<Peugeot> Peugeot { get; set; }
